fefc30e6cc25038f628f7fe7cbc729f1aae75798
ccallado <ccallado2005@gmail.com>
2013-06-25 20:22:18 +0200

    Ventana Comandos

--- ProyectoWPF1/VentanaEventos.xaml.cs
+++ ProyectoWPF1/VentanaEventos.xaml.cs
@@ -41,16 +41,6 @@ namespace ProyectoWPF1
             //MessageBox.Show(cad);
         }
 
-        private void eventoTecla(object sender, KeyEventArgs e)
-        {
-            if (((FrameworkElement)e.Source).Name == "listBox1" &&
-                e.Key==Key.Delete )
-            {
-                listBox1.Items.Clear();
-                e.Handled = true;
-            }
-        }
-
         private void button1_Click(object sender, RoutedEventArgs e)
         {
 
@@ -58,7 +48,7 @@ namespace ProyectoWPF1
 
         private void button2_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            label1.Content = "PMM - " + ((FrameworkElement )sender).Name + ": " +
+            label1.Content = "PMM - " + ((FrameworkElement)sender).Name + ": " +
                 Mouse.GetPosition(stackPanel1) + " (" +
                 ((FrameworkElement)e.Source).Name + ")";
         }
@@ -66,7 +56,7 @@ namespace ProyectoWPF1
         private void stackPanel1_PreviewMouseMove(object sender, MouseEventArgs e)
         {
             label2.Content = "PMM - " + ((FrameworkElement)sender).Name + ": " +
-                Mouse.GetPosition(stackPanel1 ) + " (" +
+                Mouse.GetPosition(stackPanel1) + " (" +
                 ((FrameworkElement)e.Source).Name + ")";
             // Si en tunneling pongo Handled = true el evento no sigue
             // ni en ventanas siguientes
@@ -89,11 +79,22 @@ namespace ProyectoWPF1
                 ((FrameworkElement)e.Source).Name + ")";
         }
 
+        private void eventoTecla(object sender, KeyEventArgs e)
+        {
+            if (((FrameworkElement)e.Source).Name == "listBox1" &&
+                e.Key == Key.Delete)
+            {
+                e.Handled = true;
+                if (e.RoutedEvent.Name == "PreviewKeyDown")
+                    listBox1.Items.Clear();
+            }
+        }
+
         private void PKD(object sender, KeyEventArgs e)
         {
             //PreviewKeyDown
-            listBox1.Items.Add((listBox1.Items.Count+1) +
-                "PKD en " + ((FrameworkElement)sender).Name + 
+            listBox1.Items.Add((listBox1.Items.Count + 1) +
+                " PKD en " + ((FrameworkElement)sender).Name +
                 " => Origen: " +
                 ((FrameworkElement)e.Source).Name);
         }
@@ -102,7 +103,7 @@ namespace ProyectoWPF1
         {
             //KeyDown
             listBox1.Items.Add((listBox1.Items.Count + 1) +
-                "KD en " + ((FrameworkElement)sender).Name +
+                " KD en " + ((FrameworkElement)sender).Name +
                 " => Origen: " +
                 ((FrameworkElement)e.Source).Name);
         }
@@ -111,7 +112,7 @@ namespace ProyectoWPF1
         {
             //PreviewKeyUp
             listBox1.Items.Add((listBox1.Items.Count + 1) +
-                "PKU en " + ((FrameworkElement)sender).Name +
+                " PKU en " + ((FrameworkElement)sender).Name +
                 " => Origen: " +
                 ((FrameworkElement)e.Source).Name);
         }
@@ -120,9 +121,83 @@ namespace ProyectoWPF1
         {
             //KeyUP
             listBox1.Items.Add((listBox1.Items.Count + 1) +
-                "KU en " + ((FrameworkElement)sender).Name +
+                " KU en " + ((FrameworkElement)sender).Name +
                 " => Origen: " +
                 ((FrameworkElement)e.Source).Name);
+        }
+
+        //No vamos a permitir que se imprima el espacio en la caja de texto2
+        //No podemos usar el evento KeyDown algunas pulsaciones no se recogen en este
+        //evento. 
+        //Se debe utilizar PreviewKeyDown
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Por aquí NUNCA entrará porque el KeyDown NO captura el espacio 
+            //(entre otras teclas)
+            //KeyDown
+            listBox1.Items.Add((listBox1.Items.Count + 1) +
+                " KD en " + ((FrameworkElement)sender).Name +
+                " => Origen: " +
+                ((FrameworkElement)e.Source).Name);
+            //if (e.Key == Key.Space)
+            //{
+            //    e.Handled = true;
+            //    listBox1.Items.Add("Detenido");
+            //}
+            //Dtengo si es un dígito del teclado numérico.
+            if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                e.Handled = true;
+                //la tecla no se pasa a la caja de texto
+                listBox1.Items.Add((listBox1.Items.Count + 1) + " Detenido");
+            }
+        }
+
+        private void textBox2_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            //PreviewKeyDown
+            listBox1.Items.Add((listBox1.Items.Count + 1) +
+                " PKD en " + ((FrameworkElement)sender).Name +
+                " => Origen: " +
+                ((FrameworkElement)e.Source).Name);
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+                //la tecla no se pasa a la caja de texto
+                listBox1.Items.Add((listBox1.Items.Count + 1) + " Detenido");
+            }
+        }
+
+        private void CambioCaja(object sender, TextChangedEventArgs e)
+        {
+            listBox1.Items.Add((listBox1.Items.Count + 1) +
+                " Cambio: " + ((TextBox)e.Source).Text);
+        }
+
+        bool tControl, tShift, tAlt;
+
+        private void chequeoCaja(object sender, KeyEventArgs e)
+        {
+            //Si las variables estubiesen definidas aquí dentro habría que inicializarlas.
+            //Al estar fuera ya vienen inicializadas a false
+
+            //Para PreviewKeyDown y PreviewKeyUp
+
+            if (e.Key == Key.LeftCtrl || e.Key == Key.RightCtrl)
+                tControl = e.IsDown;
+            label5.Content = (tControl ? "Ctrl" : "");
+
+            if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+                tShift = e.IsDown;
+            if (tShift)
+                label5.Content += (label5.Content.ToString() != "" ? "+" : "") + "Shift";
+
+            if (e.Key == Key.LeftAlt || e.Key == Key.RightAlt)
+                tAlt = e.IsDown;
+
+            if (tAlt)
+                label5.Content += (label5.Content.ToString() != "" ? "+" : "") + "Alt";
+
         }
     }
 }
